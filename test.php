65d90b589ddf3c99bb82b5e48af1b69c56223b05
unknown <eminem@.(none)>
2012-09-12 11:04:10 +0700

    a

--- test.php
+++ test.php
@@ -1,17 +1,13 @@
 <?php
 include ('image_magic.php');
 $img = new Image_magic("demo.jpg");
-//$img -> rotation(45);
-//$img -> resize("auto", 100);
+$img -> rotation(45);
+$img -> resize("auto", 100);
 //$img -> crop(0, 0, 100, 200);
 //$img->add_watermark("daulau.jpg", 50, 50);
 
 //$img->insert_text('Nguyen Trang Dong',30,'red','28 Days Later',10,10,0,'black',3);
 
-
-//$img -> save('ka1.jpg');
-
-$img->test_insert_text("lay tam cai anh hot girl ;))",30);
 
 $img -> show();
 
